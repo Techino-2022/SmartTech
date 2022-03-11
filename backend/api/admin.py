29f585379de2898a6c77b84724628b1d01c0d81c
DtechB <d.bayati28@gmail.com>
2022-03-11 18:06:36 +0330

    create post, category, postImageUrl, PostDescription and comment models.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from . import models
 
 UserAdmin.fieldsets[2][1]['fields'] = (
     'is_active', 'is_staff', 'is_superuser', 'groups', 'phone_number', 'address',
     'user_permissions', 'membership', 'avatar'
 )
 
-admin.site.register(User, UserAdmin)
+admin.site.register(models.User, UserAdmin)
+
+admin.site.register(models.Post)
+admin.site.register(models.Category)
+admin.site.register(models.PostDescription)
+admin.site.register(models.PostImgUrl)
+admin.site.register(models.Comment)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class User(AbstractUser):
@@ -18,5 +20,46 @@ class User(AbstractUser):
     address = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(unique=True)
 
+    def __str__(self):
+        return self.username
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    temp_context = models.TextField()
+    category = models.ManyToManyField(Category, related_name='post', db_table='post_category')
+    published_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='post')
+    favorite = models.ManyToManyField(User, related_name='post_favorite', blank=True, db_table='favorite_post')
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
+
+
+class PostDescription(models.Model):
+    context = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post
+
+
+class PostImgUrl(models.Model):
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True, blank=True)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
