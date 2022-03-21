09a093e0eaef0ae0124659182ffc6ee8c4de3ef3
Mostafa Fazli <MosFazli@gmail.com>
2022-03-21 16:35:43 +0330

    Phone Area (Home Screen) Design, not complete !

--- frontend/screens/Home/HomeScreen.js
+++ frontend/screens/Home/HomeScreen.js
@@ -6,6 +6,17 @@ export default function App() {
     <View style={styles.mainView}>
       <StatusBar style="auto" />
 
+
+    <View style={styles.pageTitle}>
+
+      <Text style={styles.PageName}>
+
+      Phones Area
+
+      </Text>
+
+    </View>
+
       <View style={styles.cardView}>
 
         <Image source={require('../../assets/phone_images/Samsung_S22_ultra_1.jpg')} style={styles.cardImage}>
@@ -18,6 +29,18 @@ export default function App() {
         <Text style={styles.cardTitle}>
           Samsung Galaxy Note 20
         </Text>
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Display:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        6.7" Super AMOLED Plus
+        </Text>
+
+        </View>
 
         <View style={styles.componentSecion}>
 
@@ -65,7 +88,7 @@ export default function App() {
         </Text>
 
         <Text style={styles.componentDescription}>
-        4300mAh, non-removable
+        4300 mAh, non-removable
         </Text>
 
 
@@ -87,6 +110,296 @@ export default function App() {
         </View>
 
       </View>
+
+
+
+
+    <View style={styles.cardView}>
+
+      <Image source={require('../../assets/phone_images/xiaomi-poco-x3-pro-1.jpg')} style={styles.cardImage}>
+
+
+      </Image>
+
+      <View style={styles.cardRightSection}>
+
+      <Text style={styles.cardTitle}>
+        Xiaomi Poco X3 Pro - A
+      </Text>
+
+      <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Display:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        6.67" IPS LCD
+        </Text>
+
+        </View>
+
+      <View style={styles.componentSecion}>
+
+      <Text style={styles.components}>
+      CPU:
+      </Text>
+
+      <Text style={styles.componentDescription}>
+      Qualcomm Snapdragon 860 (7 nm)
+      </Text>
+
+      </View>
+
+
+      <View style={styles.componentSecion}>
+
+      <Text style={styles.components}>
+      RAM:
+      </Text>
+
+      <Text style={styles.componentDescription}>
+      6GB
+      </Text>
+
+      </View>
+
+
+      <View style={styles.componentSecion}>
+
+      <Text style={styles.components}>
+      Storage:
+      </Text>
+
+      <Text style={styles.componentDescription}>
+      128GB UFS 3.1
+      </Text>
+
+      </View>
+
+
+      <View style={styles.componentSecion}>
+
+      <Text style={styles.components}>
+      Battery:
+      </Text>
+
+      <Text style={styles.componentDescription}>
+      5160 mAh, non-removable
+      </Text>
+
+
+      </View>
+
+      <View style={styles.componentSecion}>
+
+      <Text style={styles.components}>
+      OS:
+      </Text>
+
+      <Text style={styles.componentDescription}>
+      Android 11, MIUI 12.5 for POCO
+      </Text>
+
+      </View>
+
+      </View>
+
+
+</View>
+
+
+<View style={styles.cardView}>
+
+        <Image source={require('../../assets/phone_images/xiaomi-poco-x3-pro-1.jpg')} style={styles.cardImage}>
+
+
+        </Image>
+
+        <View style={styles.cardRightSection}>
+
+        <Text style={styles.cardTitle}>
+        Xiaomi Poco X3 Pro - B
+        </Text>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Display:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        6.67" IPS LCD
+        </Text>
+
+        </View>
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        CPU:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        Qualcomm Snapdragon 860 (7 nm)
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        RAM:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        8GB
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Storage:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        128GB UFS 3.1
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Battery:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        5160 mAh, non-removable
+        </Text>
+
+
+        </View>
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        OS:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        Android 11, MIUI 12.5 for POCO
+        </Text>
+
+        </View>
+
+
+        </View>
+
+      </View>
+
+      <View style={styles.cardView}>
+
+        <Image source={require('../../assets/phone_images/apple-iphone-13-pro-max-2.jpg')} style={styles.cardImage}>
+
+
+        </Image>
+
+        <View style={styles.cardRightSection}>
+
+        <Text style={styles.cardTitle}>
+          Apple IPhone 13 Pro Max
+        </Text>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Display:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        6.67" Super Retina XDR OLED
+        </Text>
+
+        </View>
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        CPU:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        Apple A15 Bionic (5 nm)
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        RAM:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        6GB
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Storage:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        128GB NVMe
+        </Text>
+
+        </View>
+
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        Battery:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        4352 mAh, non-removable
+        </Text>
+
+
+        </View>
+
+        <View style={styles.componentSecion}>
+
+        <Text style={styles.components}>
+        OS:
+        </Text>
+
+        <Text style={styles.componentDescription}>
+        iOS 15, can up to 15.4
+        </Text>
+
+        </View>
+
+
+        </View>
+
+      </View>
+
+
 
     </View>
   );
--- frontend/screens/Home/styles.js
+++ frontend/screens/Home/styles.js
@@ -1,23 +1,43 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+
+    PageName:{
+      color:'#F0A500',
+      fontWeight:'700',
+      textAlign:'center',
+      margin:7
+    },
+
+    pageTitle:{
+      color:'white',
+      margin:10,
+      backgroundColor: 'black',
+      width:'30%',
+      borderRadius:100,
+      borderColor: '#F0A500',
+      borderWidth:3,
+
+    },
+
     mainView: {
       flex: 1,
       alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'black'
+      justifyContent: 'flex-start',
+      backgroundColor: 'black',
     },
 
     cardTitle: {
       color: 'white',
       fontWeight:'700',
       textAlign:'center',
+      fontSize:15,
       margin:10
     },
 
     cardImage:{
       height: '98%',
-      width: '40%',
+      width: '39%',
       resizeMode:'contain',
       borderRadius: 15,
       alignSelf:'center',
@@ -26,12 +46,13 @@ const styles = StyleSheet.create({
 
     cardView: {
       backgroundColor: 'rgba(71, 71, 71, 0.7)',
-      width: '85%',
-      height: '20%',
+      width: '97%',
+      height: '22%',
       borderRadius: 10,
       flexDirection: 'row',
       borderColor: '#F0A500',
       borderWidth:3,
+      margin:4,
     },
 
 
@@ -42,7 +63,8 @@ const styles = StyleSheet.create({
     components:{
       color:'grey',
       fontWeight:'500',
-      marginRight:5
+      marginRight:5,
+      fontSize:10,
     },
 
     componentSecion: {
@@ -51,7 +73,8 @@ const styles = StyleSheet.create({
     },
 
     componentDescription:{
-      color: 'white'
+      color: 'white',
+      fontSize:11,
     }
   });
 
